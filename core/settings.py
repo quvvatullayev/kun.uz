c4f659f28b55a7fcc3cf5817e93fee3155fe2964
ogabek <quvvatullayev@gmail.com>
2023-05-11 09:01:47 +0500

    fixt

--- core/settings.py
+++ core/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "kun_uz",
-    "rest_framework"
-    "rest_framework.authtoken"
+    "rest_framework",
+    'rest_framework.authtoken',
 ]
 
 REST_FRAMEWORK = {
