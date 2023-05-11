11e6cbd7d6dfe9d0164e7a1060f7ca096d806972
ogabek <quvvatullayev@gmail.com>
2023-05-11 08:47:04 +0500

    add sitting in auth token

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "kun_uz",
     "rest_framework"
+    "rest_framework.authtoken"
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
